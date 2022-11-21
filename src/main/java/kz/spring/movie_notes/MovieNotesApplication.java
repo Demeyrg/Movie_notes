b38e8481d90ba860266dec2735725e3ec2da1043
Slava Aleinikov <slava.aleinikov@mail.ru>
2022-11-22 00:46:20 +0600

    Initial commit

---
+++ src/main/java/kz/spring/movie_notes/MovieNotesApplication.java
@@ -0,0 +1,13 @@
+package kz.spring.movie_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/spring/movie_notes/MovieNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.spring.movie_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
